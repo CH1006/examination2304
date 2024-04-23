c69eb1012fae3644f6d8246c89f13b45b2157ffc
huylc <conghuy1006@gmail.com>
2024-04-23 20:43:44 +0700

    add MQTT

--- go.mod
+++ go.mod
@@ -5,6 +5,13 @@ go 1.20
 require github.com/samber/lo v1.39.0
 
 require (
+	github.com/eclipse/paho.mqtt.golang v1.4.3 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	golang.org/x/net v0.8.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
+)
+
+require (
 	github.com/go-playground/assert/v2 v2.2.0
 	golang.org/x/exp v0.0.0-20220303212507-bbda1eaf7a17 // indirect
 )
--- main.go
+++ main.go
@@ -1,12 +1,76 @@
 package main
 
 import (
-	"examination2304/model"
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
 )
 
+var knt int
+var topic = "back-end-exam"
+var messagePubHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
+	fmt.Printf("MSG: %s\n", msg.Payload())
+	// get msg and save history when light on/off
+}
+
+var connectHandler mqtt.OnConnectHandler = func(client mqtt.Client) {
+	fmt.Println("Connected")
+}
+
+var connectLostHandler mqtt.ConnectionLostHandler = func(client mqtt.Client, err error) {
+	fmt.Printf("Connect lost: %v", err)
+}
+
 func main() {
-	log.Println(model.GetLightList([]int{200, 300, 500}, 800))
-	log.Println(model.GetLightList([]int{200, 300, 600, 700}, 800))
-	log.Println(model.GetLightList([]int{200}, 100))
+	knt = 0
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	var broker = "a1089xdb9qnyqp-ats.iot.ap-southeast-1.amazonaws.com"
+	var port = 8883
+	opts := mqtt.NewClientOptions()
+	opts.AddBroker(fmt.Sprintf("ssl://%s:%d", broker, port))
+	tlsConfig := NewTlsConfig()
+	opts.SetTLSConfig(tlsConfig)
+	opts.SetClientID("go_mqtt_client")
+	opts.SetDefaultPublishHandler(messagePubHandler)
+	opts.OnConnect = func(c mqtt.Client) {
+		if token := c.Subscribe(topic, 0, messagePubHandler); token.Wait() && token.Error() != nil {
+			panic(token.Error())
+		}
+	}
+	opts.OnConnectionLost = connectLostHandler
+	client := mqtt.NewClient(opts)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	} else {
+		fmt.Printf("Connected to server\n")
+	}
+	<-c
+}
+
+func NewTlsConfig() *tls.Config {
+	certpool := x509.NewCertPool()
+	ca, err := os.ReadFile("root_ca.pem")
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	certpool.AppendCertsFromPEM(ca)
+	// Import client certificate/key pair
+	clientKeyPair, err := tls.LoadX509KeyPair("certificate.crt", "private_key.key")
+	if err != nil {
+		panic(err)
+	}
+	return &tls.Config{
+		RootCAs:            certpool,
+		ClientAuth:         tls.NoClientCert,
+		ClientCAs:          nil,
+		InsecureSkipVerify: true,
+		Certificates:       []tls.Certificate{clientKeyPair},
+	}
 }
